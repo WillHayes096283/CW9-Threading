da0fa4d5bb5198de6017d415a565d97d84289b32
William Hayes <wilthaye@ut.utm.edu>
2023-03-01 11:31:15 -0600

    Got the Threads created along with the loops.

---
+++ CW9/FindPiThread.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW9
+{
+    public class FindPiThread
+    {
+        int numDarts;
+        int count;
+
+        Random rnd;
+
+        public FindPiThread(int numDarts)
+        {
+            rnd = new Random();
+
+            this.numDarts = numDarts;
+            this.count = 0;
+        }
+
+        public int getCount() 
+        {
+            return count;
+        }
+
+        public void throwDarts()
+        {
+            while (numDarts > 0)
+            {
+                double x = rnd.Next(0, 6);
+                double y = rnd.Next(0, 6);
+
+                double val = Math.Sqrt((x * x) + (y * y));
+                if (val <= 1.0)
+                {
+                    numDarts--;
+                    count++;
+                }
+
+                else
+                {
+                    numDarts--;
+                }
+            }
+        }
+
+
+
+    }
+}
--- CW9/Program.cs
+++ CW9/Program.cs
@@ -2,14 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CW9
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the number of throws each thread should make: ");
+            int numThrows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the number of threads to make: ");
+            int numThread = Convert.ToInt32(Console.ReadLine());
+
+            List<FindPiThread> piList = new List<FindPiThread>();
+            List<Thread> threads = new List<Thread>();
+
+            while (numThread > 0)
+            {
+                FindPiThread finder = new FindPiThread(numThrows);
+                piList.Add(finder);
+
+                Thread threadObj = new Thread(new ThreadStart(finder.throwDarts));
+                threads.Add(threadObj);
+
+                threadObj.Start();
+                Thread.Sleep(16);
+
+                numThread--;
+            }
+
+            foreach(Thread t in threads)
+            {
+                t.Join();
+            }
+
+            foreach(FindPiThread p in piList)
+            {
+                int cnt = p.getCount();
+                
+                double eval = (4.0 * (Convert.ToDouble(cnt) / Convert.ToDouble(numThrows)));
+
+                Console.WriteLine("Evaulation: " + eval);
+            }
+
+            Console.ReadKey();
         }
     }
 }
